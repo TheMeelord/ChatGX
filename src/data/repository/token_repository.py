188cda247e777ff1efbacfe509945f754776f9da
TheMeelord <misha.laptev.03@mail.ru>
2024-05-28 00:10:12 +0800

    add get_user_id_by_token and token generation

--- src/data/repository/token_repository.py
+++ src/data/repository/token_repository.py
@@ -1,3 +1,5 @@
+import secrets
+
 from sqlalchemy.orm import Session
 from time import gmtime
 from src.data.dbo.TokenDbo import TokenDbo
@@ -11,8 +13,13 @@ class token_repository:
         return self.db.query(TokenDbo).filter(TokenDbo.user_id == user_id).first()
 
     async def create_token(self, user_id):
-        token = TokenDbo(user_id=user_id, token=hash(f"{user_id}{gmtime()}"))
+        token_str = secrets.token_urlsafe(32)
+        token = TokenDbo(user_id=user_id, token=token_str)
         self.db.add(token)
         self.db.commit()
         self.db.refresh(token)
         return token
+
+    async def get_user_id_by_token(self, token_str):
+        token = self.db.query(TokenDbo).filter(TokenDbo.token == token_str).first()
+        return token.user_id if token else None
