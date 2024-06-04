7ac7ca1cba02864062ed6258dce5f37b4e62349e
TheMeelord <misha.laptev.03@mail.ru>
2024-06-04 22:45:29 +0800

    update scripts and file

--- client/script.js
+++ client/script.js
@@ -1,13 +1,13 @@
 function getCookie(name) {
-const cookies = document.cookie.split(';');
-for (let i = 0; i < cookies.length; i++) {
-    const cookie = cookies[i].trim();
-    // Проверяем, начинается ли текущий cookie с имени, за которым следует "="
-    if (cookie.startsWith(name + '=')) {
-    return cookie.substring(name.length + 1);
-    }
-}
-return null;
+  const cookies = document.cookie.split(';');
+  for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      // Проверяем, начинается ли текущий cookie с имени, за которым следует "="
+      if (cookie.startsWith(name + '=')) {
+          return cookie.substring(name.length + 1);
+      }
+  }
+  return null;
 }
 
 var CHAT_ID = null;
@@ -17,137 +17,146 @@ const baseUrl = "ws://127.0.0.1:5000/ws/connect/";
 let socket;
 
 function initWebSocket(token) {
-const socketUrl = `${baseUrl}${token}`;
-socket = new WebSocket(socketUrl);
+  const socketUrl = `${baseUrl}${token}`;
+  socket = new WebSocket(socketUrl);
 
 
-socket.onopen = function(event) {
-    console.log("WebSocket соединение установлено.");
-};
+  socket.onopen = function(event) {
+      console.log("WebSocket соединение установлено.");
+  };
 
-socket.onmessage = function(event) {
-    console.log("Получено сообщение:", event.data);
-    if(message.includes("new message")){
-      const message = event.data.split(" ");
-      let chat_id = parseInt(message[0]);
-      console.log(chat_id);
-      if (chat_id == CHAT_ID){
-        loadChat(chat_id);
+  socket.onmessage = function(event) {
+      console.log("Получено сообщение:", event.data);
+      if (event.data.includes("new message")) {
+          const message = event.data.split(" ");
+          let chat_id = parseInt(message[0]);
+          console.log(chat_id);
+          if (chat_id == CHAT_ID) {
+              loadChat(chat_id);
+          }
+
+          return;
       }
+      const message = event.data.split(" ");
+      const friend_id = parseInt(message[0]);
+      const status = message[1];
 
-      return;
-    }
-    const message = event.data.split(" ");
-    const friend_id = parseInt(message[0]);
-    const status = message[1];
+      if (status === "connected") {
+          updateFriendStatus(friend_id, "online");
+      } else if (status === "disconnected") {
+          updateFriendStatus(friend_id, "offline");
+      }
+  };
 
-    if (status === "connected") {
-        updateFriendStatus(friend_id, "online");
-    } else if (status === "disconnected") {
-        updateFriendStatus(friend_id, "offline");
-    }
-};
+  socket.onclose = function(event) {
+      console.log("WebSocket соединение закрыто.");
+  };
 
-socket.onclose = function(event) {
-    console.log("WebSocket соединение закрыто.");
-};
-
-socket.onerror = function(event) {
-    console.error("Произошла ошибка WebSocket соединения:", event);
-};
+  socket.onerror = function(event) {
+      console.error("Произошла ошибка WebSocket соединения:", event);
+  };
 }
-  // Функция для получения списка чатов с сервера
+// Функция для получения списка чатов с сервера
 function getChats(token) {
-    const apiUrl = `http://127.0.0.1:5000/chat/get_all/${token}`;
-  
-    $.ajax({
+  const apiUrl = `http://127.0.0.1:5000/chat/get_all/${token}`;
+
+  $.ajax({
       type: "GET",
       url: apiUrl,
       success: function(response) {
-        // Обработка успешного ответа от сервера
-        if (response && response.chats) {
-          response.chats.forEach(chat => {
-            addFriendToChatList(chat.id, chat.friend_id, chat.friend_username, 0, chat.friend_status);
-          });
-        } else {
-          console.error("Ошибка: Некорректный формат ответа сервера.");
-        }
+          // Обработка успешного ответа от сервера
+          if (response && response.chats) {
+              response.chats.forEach(chat => {
+                  addFriendToChatList(chat.id, chat.friend_id, chat.friend_username, 0, chat.friend_status);
+              });
+              // При загрузке страницы восстанавливаем состояние
+            const savedChatId = getCookie('activeChatId');
+            if (savedChatId) {
+              console.log("Restored chat ID:", savedChatId);
+              selectFriendById(savedChatId);
+            }
+          } else {
+              console.error("Ошибка: Некорректный формат ответа сервера.");
+          }
       },
       error: function(xhr, status, error) {
-        // Обработка ошибок
-        console.error("Ошибка при получении списка чатов:", error);
+          // Обработка ошибок
+          console.error("Ошибка при получении списка чатов:", error);
       }
-    });
-  }
-  
+  });
+}
+
 
 // Инициализация WebSocket при загрузке страницы
 $(document).ready(function() {
-// Получаем токен из куки
-const token = getCookie('token');
+  // Получаем токен из куки
+  const token = getCookie('token');
 
-// Проверяем наличие токена и инициализируем WebSocket при его наличии
-if (token) {
-    initWebSocket(token);
-    getChats(token);
-} else {
-    console.error("Ошибка: Токен не найден в куки.");
-}
+  // Проверяем наличие токена и инициализируем WebSocket при его наличии
+  if (token) {
+      initWebSocket(token);
+      getChats(token);
+  } else {
+      console.error("Ошибка: Токен не найден в куки.");
+  }
 });
 
 $(document).ready(function() {
-    // Функция для загрузки списка пользователей и добавления их в модальное окно
-    function loadUsers(token) {
-        const apiUrl = `http://127.0.0.1:5000/user/get_all/${token}`;
-        
-        $.ajax({
-            type: "GET",
-            url: apiUrl,
-            success: function(response) {
-                // Очищаем список пользователей
-                $('#userList').empty();
-                // Добавляем каждого пользователя в список
-                response.users.forEach(user => {
-                    $('#userList').append(`<li class="list-group-item" data-user-id="${user.id}" data-dismiss="modal">${user.username}</li>`);
-                });
-            },
-            error: function(xhr, status, error) {
-                console.error("Ошибка при получении списка пользователей:", error);
-            }
-        });
-    }
-    
-    // Событие открытия модального окна для создания чата
-    $('#openCreateChatModalBtn').click(function() {
-        // Получаем токен из куки
-        const token = getCookie('token');
-        if (token) {
-            loadUsers(token);
-        } else {
-            console.error("Ошибка: Токен не найден в куки.");
-        }
-    });
-    
-    // Событие выбора пользователя для создания чата
-    $('#userList').on('click', 'li', function() {
-        const userId = $(this).data('user-id');
-        const token = getCookie('token');
-        
-        // Отправляем запрос на создание чата
-        $.ajax({
-            type: "POST",
-            url: "http://127.0.0.1:5000/chat/create",
-            contentType: "application/json",
-            data: JSON.stringify({ friend_id: userId, token: token }),
-            success: function(response) {
-                console.log("Чат успешно создан:", response);
-                // Здесь можно добавить логику для обновления интерфейса с новым чатом
-            },
-            error: function(xhr, status, error) {
-                console.error("Ошибка при создании чата:", error);
-            }
-        });
-    });
+  // Функция для загрузки списка пользователей и добавления их в модальное окно
+  function loadUsers(token) {
+      const apiUrl = `http://127.0.0.1:5000/user/get_all/${token}`;
+
+      $.ajax({
+          type: "GET",
+          url: apiUrl,
+          success: function(response) {
+              // Очищаем список пользователей
+              $('#userList').empty();
+              // Добавляем каждого пользователя в список
+              response.users.forEach(user => {
+                  $('#userList').append(`<li class="list-group-item" data-user-id="${user.id}" data-dismiss="modal">${user.username}</li>`);
+              });
+          },
+          error: function(xhr, status, error) {
+              console.error("Ошибка при получении списка пользователей:", error);
+          }
+      });
+  }
+
+  // Событие открытия модального окна для создания чата
+  $('#openCreateChatModalBtn').click(function() {
+      // Получаем токен из куки
+      const token = getCookie('token');
+      if (token) {
+          loadUsers(token);
+      } else {
+          console.error("Ошибка: Токен не найден в куки.");
+      }
+  });
+
+  // Событие выбора пользователя для создания чата
+  $('#userList').on('click', 'li', function() {
+      const userId = $(this).data('user-id');
+      const token = getCookie('token');
+
+      // Отправляем запрос на создание чата
+      $.ajax({
+          type: "POST",
+          url: "http://127.0.0.1:5000/chat/create",
+          contentType: "application/json",
+          data: JSON.stringify({
+              friend_id: userId,
+              token: token
+          }),
+          success: function(response) {
+              console.log("Чат успешно создан:", response);
+              // Здесь можно добавить логику для обновления интерфейса с новым чатом
+          },
+          error: function(xhr, status, error) {
+              console.error("Ошибка при создании чата:", error);
+          }
+      });
+  });
 });
 
 function loadChat(chat_id) {
@@ -155,61 +164,64 @@ function loadChat(chat_id) {
   const user_id = getCookie('user_id');
   CHAT_ID = chat_id;
   fetch(`http://127.0.0.1:5000/chat/history/${token}/${chat_id}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Chat not found');
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log(data);
-      // Обработка полученных данных чата
-      const chatContainer = document.getElementById('chat');
-      chatContainer.innerHTML = ''; // Очистить содержимое чата перед загрузкой новых данных
-      data.messages.forEach(message => {
-        const messageElement = document.createElement('div');
-        messageElement.textContent = message.text;
-        messageElement.classList.add('message-container');
-        if (message.sender_id!=user_id){
-            messageElement.classList.add('sender-message');
-        }
-        else{
-            messageElement.classList.add('receiver-message')
-        }
-        chatContainer.appendChild(messageElement);
+      .then(response => {
+          if (!response.ok) {
+              throw new Error('Chat not found');
+          }
+          return response.json();
+      })
+      .then(data => {
+          console.log(data);
+          // Обработка полученных данных чата
+          const chatContainer = document.getElementById('chat');
+          chatContainer.innerHTML = ''; // Очистить содержимое чата перед загрузкой новых данных
+          data.messages.forEach(message => {
+              const messageElement = document.createElement('div');
+              messageElement.textContent = message.text;
+              messageElement.classList.add('message-container');
+              if (message.sender_id != user_id) {
+                  messageElement.classList.add('sender-message');
+              } else {
+                  messageElement.classList.add('receiver-message')
+              }
+              chatContainer.appendChild(messageElement);
+          });
+          chatContainer.scrollTop = chatContainer.scrollHeight;
+      })
+      .catch(error => {
+          alert("Ошибка: " + error.message);
       });
-    })
-    .catch(error => {
-      alert("Ошибка: " + error.message);
-    });
+      document.cookie = `activeChatId=${chat_id}; path=/`;
 }
 
-function sendMessage(){
+function sendMessage() {
   const token = getCookie('token');
 
   fetch("http://127.0.0.1:5000/chat/send", {
-    method: 'POST',
-    headers: {
-      'Content-Type' : 'application/json'
-    },
-    body: JSON.stringify({
-      chat_id: CHAT_ID,
-      token: token,
-      text: document.getElementById('inputText').value
-    })
-}).then(response => {
-  if (!response.ok) {
-    throw new Error('Chat not found');
-  }
-  return response.json();
-})
-.then(data => {console.log(data)})
-const chatContainer = document.getElementById('chat');
-const messageElement = document.createElement('div');
-        messageElement.textContent = document.getElementById('inputText').value;
-        messageElement.classList.add('message-container');
-        chatContainer.appendChild(messageElement);
-        document.getElementById('inputText').value = '';
-
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+              chat_id: CHAT_ID,
+              token: token,
+              text: document.getElementById('inputText').value
+          })
+      }).then(response => {
+          if (!response.ok) {
+              throw new Error('Chat not found');
+          }
+          return response.json();
+      })
+      .then(data => {
+          console.log(data)
+      })
+  const chatContainer = document.getElementById('chat');
+  const messageElement = document.createElement('div');
+  messageElement.textContent = document.getElementById('inputText').value;
+  messageElement.classList.add('message-container');
+  messageElement.classList.add('receiver-message');
+  chatContainer.appendChild(messageElement);
+  document.getElementById('inputText').value = '';
+  chatContainer.scrollTop = chatContainer.scrollHeight;
 }
-
--- client/ui_scripts.js
+++ client/ui_scripts.js
@@ -1,30 +1,48 @@
-// Пример данных друзей
-let friends = [
-    { id: 1, username: "Пользователь1", unread_messages: 0, status: "Онлайн" },
-    { id: 2, username: "Пользователь2", unread_messages: 0, status: "Офлайн" },
-    // Добавьте дополнительных друзей при необходимости
-  ];
+let currentChatId = null;
 
-  // Функция для добавления друга в список чата
 function addFriendToChatList(chat_id, friend_id, friend_username, unread_messages, status) {
-    const friendList = document.getElementById("friendList");
-    const friendDiv = document.createElement("a");
-    friendDiv.href = "#";
-    friendDiv.classList.add("list-group-item", "list-group-item-action");
-    friendDiv.dataset.chatId = chat_id; // Добавляем атрибут data для chat_id
-    friendDiv.dataset.friendId = friend_id; // Добавляем атрибут data для friend_id
-    friendDiv.innerHTML = `
-      <div class="d-flex w-100 justify-content-between">
-        <h5 class="mb-1">${friend_username}</h5>
-        <small>${status}</small>
-      </div>
-      <small>Непрочитанных сообщений: <span class="unread-messages">${unread_messages}</span></small>
-    `;
-    friendDiv.addEventListener("click", () => loadChat(chat_id)); // Загрузка чата при клике на друга
-    friendList.appendChild(friendDiv);
+  const friendList = document.getElementById("friendList");
+  const friendDiv = document.createElement("a");
+  friendDiv.href = "#";
+  friendDiv.classList.add("list-group-item", "list-group-item-action", "friend");
+  friendDiv.dataset.chatId = chat_id; // Добавляем атрибут data для chat_id
+  friendDiv.dataset.friendId = friend_id; // Добавляем атрибут data для friend_id
+  friendDiv.innerHTML = `
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1">${friend_username}</h5>
+    </div>
+    <small>${status}</small>
+  `;
+  friendDiv.addEventListener("click", () => selectFriend(friendDiv, chat_id)); // Загрузка чата при клике на друга
+  friendList.appendChild(friendDiv);
 }
 
-  
+
+function selectFriendById(chat_id) {
+  const friendElements = document.querySelectorAll('.friend');
+  friendElements.forEach(friend => {
+    if (friend.dataset.chatId === chat_id) {
+      selectFriend(friend, chat_id);
+    }
+  });
+}
+
+function selectFriend(element, chat_id) {
+  // Убираем класс active у всех друзей
+  const friends = document.querySelectorAll('.friend');
+  friends.forEach(friend => friend.classList.remove('active'));
+
+  // Добавляем класс active текущему другу
+  element.classList.add('active');
+
+  // Устанавливаем текущий чат ID
+  currentChatId = chat_id;
+
+  // Здесь загружаем историю сообщений с выбранным пользователем
+  loadChat(chat_id);
+  console.log(`Chat with user ID ${currentChatId} selected`);
+}
+
 function updateFriendStatus(friend_id, new_status) {
     console.log(`Статус друга ${friend_id} обновлен на ${new_status}`);
     const friendDiv = document.querySelector(`a[data-friend-id="${friend_id}"]`);
@@ -32,12 +50,3 @@ function updateFriendStatus(friend_id, new_status) {
         friendDiv.querySelector("small").innerText = new_status;
     }
 }
-
-
-  // Функция загрузки чата (пока заглушка)
-  /*function loadChat(chat_id) {
-    alert("Загрузка чата с ID: " + chat_id);
-  }*/
-
-
-  
